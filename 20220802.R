168056edced2a9fe5ff64b9744e0ed6c30917fb2
estellaseo <seohm2001@gmail.com>
2022-08-03 17:37:28 +0900

    plot, barplot, palettes, boxplot, histogram, piechart

--- 20220802.R
+++ 20220802.R
@@ -2,6 +2,7 @@
 
 #1. 선그래프(산점도)
 #reference: https://www.learnbyexample.org/r-plot-function/
+#RGB color chart: https://www.rapidtables.com/web/color/RGB_Color.html
   
  plot(x,                     #X축 좌표(스칼라, 벡터)
       y,                     #Y축 좌표(스칼라, 벡터)
@@ -17,7 +18,9 @@
       cex = ,                #점크기
       lwd = ,                #선 굵기
       ann = F,               #제목 출력 여부            
-      axes = F,              #축 눈금 출력 여부
+      axes = F,              #축 전체 출력 여부
+      xaxt = 'n',            #x축 눈금 출력 x 
+      yaxt = 'n',            #y축 눈금 출력 x  
       ... )
 
 help(plot)
@@ -166,7 +169,6 @@ legend('topleft', legend=unique(iris$Species),
 
 #2. 막대 그래프
 #범주형 자료에 대한 비교 시각화 기법
-help(barplot)
 
 barplot(height,             #막대높이(벡터, 데이터프레임)
         width = 1,          #막대너비(벡터 전달 가능)
@@ -223,6 +225,155 @@ barplot(as.matrix(t1), beside = T,
         cex.axis = 0.8,
         cex.lab = 1.5,
         col.lab = 5)
+
+
+
+
+#[ 참고 ] 외부 팔레트 사용
+library(colorspace)
+hcl_palettes()                      #팔레트 종류 확인하기
+dev.new()
+hcl_palettes(plot = T)              #전체 팔레트 색상 확인
+hcl_palettes('sequential', plot = T)#특정 팔레트 색상 확인
+
+qualitative_hcl(7, palette = 'Pastel 1')
+
+
+
+#하나의 figure에 여러 그래프 그리기
+dev.new()
+par(mfrow = c(1, 3))
+
+
+#예제)
+barplot(c(120, 50, 140, 230, 140),
+          col = qualitative_hcl(5, palette = 'Pastel 1'))
+barplot(c(120, 50, 140, 230, 140),
+        col = sequential_hcl(5, palette = 'Purple-Blue'))
+barplot(c(120, 50, 140, 230, 140),
+        col = diverging_hcl(5, palette = 'Blue-Red'))
+
+
+
+
+
+#3. 상자 수염 그림 Box plot
+#연속형 변수(숫자형) 분포 확인, 비교
+#이상치 검정 시각화 기법
+#최대, 최소, 이상치, 중앙값, 사분위수, 범위(IQR)
+#IQR: Q3 - Q1
+
+#이상치 정의
+#x >= Q3 + 1.5 * IQR (정상치 최대 범위)
+#x <= Q1 - 1.5 * IQR (정상치 최소 범위)
+
+#요약정보
+summary(iris[, -5])
+
+dev.new()
+boxplot(iris[, -5])
+
+
+
+
+
+#4. 히스토그램 Histogram
+#연속형 변수의 분포 확인을 위한 시각화 기법
+#도수분포표를 시각화한 것
+
+hist(x,                             #연속형 변수(벡터)
+     breaks = 'Sturges',            #계급 구간(벡터)
+     freq = NULL,                   #빈도수 기반
+     probability = !freq,           #확률 기반
+     include.lowest = T,            #첫 구간 시작 값(최소값) 구간 포함 여부
+     right = T,                     #오른쪽 닫힘 여부(이하)
+     ...)
+
+
+vheight <- c(180, 172, 168, 177, 182, 164, 161, 177, 160, 171, 166, 163)
+dev.new()
+hist(vheight, breaks = seq(160, 185, 5), pro = T) #pro: 밀집도(확률)
+
+
+
+#kde: 커널 밀도 함수(히스토그램 분포화)
+install.packages('ks')
+library(ks)
+v1 <- kde(vheight, positive = T)
+plot(v1, col = col1[3])
+
+v2 <- 2^rnorm(100)
+v2_kde <- kde(v2, positive = T)
+plot(v2_kde, col = col1[2])
+
+
+
+
+
+#5. 파이차트 Pie Chart
+#비교 시각화 기법
+
+pie(x,                              #각 파이 크기(벡터) 
+    labels = names(x),              #파이 이름(벡터)
+    edges = 200, 
+    radius = 0.8,
+    clockwise = FALSE, 
+    init.angle = ,
+    ...)
+
+dev.new()
+pie(c(30, 70), labels = c('남', '여'), 
+    radius = 0.4, init.angle = 90, clockwise = T,
+    col = col1[3:4], density = 30, border = 1)
+
+
+#3D 파이차트
+install.packages('plotrix')
+library(plotrix)
+
+pie3D(x,                            #파이 크기(벡터)
+      height = ,                    #파이 높이
+      labels = ,                    #파이 이름
+      labelcex = ,                  #글자 크기
+      labelcol = ,                  #글자 색
+      explode = ,                   #파이 간격
+      ...)
+
+v1 <- c(10, 20, 30, 40)
+v2 <- c('월', '화', '수', '목')
+v3 <- str_c(v2, '\n', v1, '%')
+
+par(mfrow = c(1, 1))
+
+lp <- pie3D(v1, height = 0.1, col = col1[1:4], labels = v3, 
+            labelcex = 1, main = '요일별 파이차트', explode = 0.05)
+
+
+#[ 참고 ] 라벨 좌표값 수동 설정
+lp                                  #각 파이별 라벨 위치 확인
+lp[1] <- 0.4                        #첫번째 파일 라벨 위치 좌표값 설정
+lp[3] <- 1.8
+
+pie3D(v1, height = 0.1, col = col1[1:4], labels = v3, 
+      labelcex = 1, main = '요일별 파이차트', explode = 0.05)
+
+
+
+#[ 참고 ] 그래프 바깥에 범례 표시 
+library(googleVis)
+m1 <- dcast(Fruits, Fruit ~ Year, value.var = 'Sales')
+
+dev.new()
+par('mar')                          #현재 설정된 여백 값 확인
+par(mar = c(5, 4, 4, 8),
+    xpd = T)                        #여백 값 변경 
+
+barplot(as.matrix(m1[-1]), beside = T,
+        col = col1[2:4], ylim = c(0, 120))
+
+legend('topright', legend = m1$Fruit,
+       fill = col1[2:4], inset = c(-0.3, 0.5))  #기존 위치에서 조절(x,y 좌표)
+
 
 
 
