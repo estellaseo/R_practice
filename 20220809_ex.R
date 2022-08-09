14fbf7ffb405d7f73883dbb592f2885b11ce0a2d
estellaseo <seohm2001@gmail.com>
2022-08-09 17:30:36 +0900

    initial update

--- 20220809_ex.R
+++ 20220809_ex.R
@@ -89,7 +89,7 @@ ex1_d[rank(ex1_d$총주문금액, ties.method = 'min'), ][1:abs(nrow(ex1_d)*0.3)
 # 2. center_grade.csv 파일에는 특정 센터의 연도별 월별 평가 등급이 저장되어 있다.
 # 각 평가 등급별로 점수가 다음과 같이 부여된다고 했을 때
 # (a등급 : 100, b등급: 110, .... 알파벳 순으로 10점씩 높은 점수가 부여)
-ex2 <- read.csv('center_grade.csv', fileEncoding = 'cp949')
+ex2 <- read.csv('data/center_grade.csv', fileEncoding = 'cp949')
 head(ex2)
 
 getmode <- function(v) {
@@ -98,8 +98,37 @@ getmode <- function(v) {
 }
 getmode(c(1,1,1,1,2,3))    # 최빈값 확인
 
-# 1) 중복 제거한 값이 몇 개인지 확인
-# 2) 결측치는 전체 데이터의 최빈값으로 대치
-# 3) 점수가 가장 낮은 연도와 점수가 가장 높은 연도의 두 점수의 차이를 출력
-# 4) 센터의 최종 점수를 출력
 
+# 1) 중복 제거한 값이 몇 개인지 확인(unique value)
+library(reshape2)
+ex2_m <- melt(ex2, id.vars = '월', variable.name = '년도', value.name = '등급')
+ex2_m$년도 <- as.numeric(str_sub(ex2_m$년도, 2, 5))
+head(ex2_m)
+
+length(unique(ex2_m$등급))
+
+# 2) 결측치는 전체 데이터의 최빈값으로 대치
+library(stringr)
+
+ex2_m$등급[str_detect(ex2_m$등급, '\\.')] <- getmode(ex2_m$등급)
+ex2[sapply(ex2, str_detect, '\\.')] <- getmode(ex2_m$등급)
+
+
+# 3) 점수가 가장 낮은 연도와 점수가 가장 높은 연도의 두 점수의 차이를 출력
+#    (연도별 총합 기준)
+colnames(ex2)[-1] <- str_sub(colnames(ex2)[-1], 2, 5)
+ex2
+
+bogi <- unique(ex2_m$등급)[order(unique(ex2_m$등급))]
+
+f_score <- function(x) {
+  vscore <- c()
+  for (i in x) {
+    vscore <- c(vscore, 100 + (which(bogi == i) - 1) * 10)
+  }
+  return(sum(vscore))
+}
+f_score(ex2$`2000`)
+
+
+# 4) 센터의 최종 점수를 출력(전체 총합 )
